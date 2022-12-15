a8dac554fbcb2961059e199319d05e964733c06b
youngil <youngil@placeb.com>
2022-12-15 17:37:46 +0900

    코드 비하인드파일로 변경

---
+++ ArticleApp/Pages/Articles/Create.razor.cs
@@ -0,0 +1,26 @@
+﻿using ArticleApp.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ArticleApp.Pages.Articles
+{
+    public partial class Create
+    {
+        [Inject]
+        NavigationManager? NavigationManager { get; set; }
+
+        [Inject]
+        IArticleRepository? ArticleRepository { get; set; }
+
+        public Article Model { get; set; } = new Article();
+
+        protected async Task btnSubmit_Click()
+        {
+            if (ArticleRepository != null)
+            {
+                await ArticleRepository.AddAsync(Model);
+            }
+
+            NavigationManager?.NavigateTo("/Articles");
+        }
+    }
+}
---
+++ ArticleApp/Pages/Articles/Index.razor.cs
@@ -0,0 +1,51 @@
+﻿using ArticleApp.Models;
+using BWBlazor;
+using Microsoft.AspNetCore.Components;
+
+namespace ArticleApp.Pages.Articles
+{
+    public partial class Index
+    {
+        [Inject]
+        IArticleRepository? ArticleRepository { get; set; }
+
+        [Inject]
+        NavigationManager? NavigationManager { get; set; }
+
+        private List<Article>? articles;
+
+        private PagerModel pagerModel = new PagerModel()
+        {
+            Size = 2,
+            ButtonCount = 5
+        };
+
+        protected override async Task OnInitializedAsync()
+        {
+            if(ArticleRepository != null)
+            {
+                pagerModel.RecordCount = await ArticleRepository.GetTotalRecordsCountAsync();
+            }
+            LoadPages();
+        }
+
+        async void LoadPages()
+        {
+            if(ArticleRepository != null )
+            {
+                articles = await ArticleRepository.GetPageAsync(pagerModel.Index, pagerModel.Size);
+            }
+            StateHasChanged();
+        }
+
+        void OnPageIndexChanged(int pageNumber)
+        {
+            LoadPages();
+        }
+
+        private void btnTitle_Click(int id)
+        {
+            NavigationManager?.NavigateTo($"/Articles/Details/{id}");
+        }
+    }
+}
